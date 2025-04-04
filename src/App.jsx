84e0376ad2cae82ae78b4f14b5d3741022b63283
sebastianosalzillo <salzillosebastiano@gmail.com>
2025-04-04 16:55:03 +0200

    feat : progetto base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleGenAI } from '@google/genai';
+
+
+function App() {
+  
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
